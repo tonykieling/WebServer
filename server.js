19ba5b1c8be36ee88a8e15649a5b44c2cbc60ce9
Tony Kieling <tony.kieling@gmail.com>
2019-01-14 03:28:21 +0000

    codeX

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get('/', function(req, res) {
   var drinks = [
       { name: 'Bloody Mary', drunkness: 3 },
       { name: 'Martini', drunkness: 5 },
-      { name: 'Scotch', drunkness: 10 }
+      { name: 'Scotch', drunkness: 10 },
+      { name: 'WATER!!', drunkness: 0}
   ];
   var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
